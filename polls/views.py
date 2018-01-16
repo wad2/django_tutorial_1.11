ba27f72440fb1eb68a42ed900015b58da347fab3
David Manlove <david.manlove@glasgow.ac.uk>
2018-01-16 10:45:18 +0000

    Up to end of Tutorial 5

--- polls/views.py
+++ polls/views.py
@@ -1,19 +1,20 @@
 from django.urls import reverse
 from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponseRedirect
+from django.utils import timezone
 from .models import Choice, Question
 
 def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:3]
+    latest_question_list = Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:3]
     context = {'latest_question_list': latest_question_list}
     return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
-   question = get_object_or_404(Question, pk=question_id)
+   question = get_object_or_404(Question.objects.filter(pub_date__lte=timezone.now()), pk=question_id)
    return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
-   question = get_object_or_404(Question, pk=question_id)
+   question = get_object_or_404(Question.objects.filter(pub_date__lte=timezone.now()), pk=question_id)
    return render(request, 'polls/results.html', {'question': question})
 
 def vote(request, question_id):
