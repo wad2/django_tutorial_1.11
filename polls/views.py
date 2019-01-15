c5badf6bfdb9fa3c63592eeca90c9f07ff04beeb
David Manlove <david.manlove@glasgow.ac.uk>
2019-01-15 13:37:17 +0000

    Merging changes with repo (2)

--- polls/views.py
+++ polls/views.py
@@ -2,30 +2,6 @@ from django.urls import reverse
 from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponseRedirect
 from django.utils import timezone
-<<<<<<< HEAD
-from django.views import generic
-from .models import Choice, Question
-
-class IndexView(generic.ListView):
-    template_name = 'polls/index.html'
-    context_object_name = 'latest_question_list'
-
-    def get_queryset(self):
-        # Return the last three published questions
-        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:3]
-
-class DetailView(generic.DetailView):
-    model = Question
-    template_name = 'polls/detail.html'
-
-    def get_queryset(self):
-        # Excludes any questions that aren't published yet
-        return Question.objects.filter(pub_date__lte=timezone.now())
-
-class ResultsView(generic.DetailView):
-    model = Question
-    template_name = 'polls/results.html'
-=======
 from .models import Choice, Question
 
 def index(request):
@@ -40,7 +16,6 @@ def detail(request, question_id):
 def results(request, question_id):
    question = get_object_or_404(Question.objects.filter(pub_date__lte=timezone.now()), pk=question_id)
    return render(request, 'polls/results.html', {'question': question})
->>>>>>> 082cfaecc7804e96e9c162db17cbd14c9ce69de8
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
